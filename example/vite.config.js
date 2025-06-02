4b6802989316c882c06558ec1c665513361447d2
Rogerio Amorim <rogerio.amorim@caixa.gov.br>
2025-06-02 15:50:55 -0300

    fix chartjs cdn url

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/YOUR_REPOSITORY_NAME/',
+  base: '/react-use-lazy-script-loader/',
   plugins: [react(), tailwindcss()],
 })
